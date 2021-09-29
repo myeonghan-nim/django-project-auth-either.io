425a36053ee564119bd7224c8f01cd0f0ef4e20d
myeonghan-nim <ataraxia9408@gmail.com>
2021-09-29 21:29:14 +0900

    Fix: account app logic

--- account/views.py
+++ account/views.py
@@ -1,21 +1,14 @@
-from django.shortcuts import render, redirect
-
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
-
-# Create your views here.
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, redirect
 
 
 def signup(request):
-
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
-
         if form.is_valid():
             form.save()
-
             return redirect('question:index')
     else:
         form = UserCreationForm()
@@ -26,26 +19,22 @@ def signup(request):
 
     return render(request, 'account/form.html', context)
 
-def login(request):
 
+def login(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, request.POST)
-
         if form.is_valid():
             auth_login(request, form.get_user())
-
-        return redirect('question:index')
+            return redirect('question:index')
     else:
         form = AuthenticationForm()
 
     context = {
         'form': form,
     }
-
     return render(request, 'account/form.html', context)
 
+
 def logout(request):
-
     auth_logout(request)
-
     return redirect('question:index')
